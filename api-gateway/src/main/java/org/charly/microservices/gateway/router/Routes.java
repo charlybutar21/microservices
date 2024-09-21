25580e49c2e4c758f3cd2c8f340694ce15de0fcb
Charly Micolas <charlymicolasbutar@gmail.com>
2024-09-21 21:36:39 +0700

    feat: add api gateway

---
+++ api-gateway/src/main/java/org/charly/microservices/gateway/router/Routes.java
@@ -0,0 +1,44 @@
+package org.charly.microservices.gateway.router;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class Routes {
+
+    @Value("${product.service.url}")
+    private String productServiceUrl;
+    @Value("${order.service.url}")
+    private String orderServiceUrl;
+    @Value("${inventory.service.url}")
+    private String inventoryServiceUrl;
+
+    @Bean
+    public RouterFunction<ServerResponse> productServiceRoute() {
+        return GatewayRouterFunctions.route("product_service")
+                .route(RequestPredicates.path("/api/product"), HandlerFunctions.http(productServiceUrl))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceRoute() {
+        return GatewayRouterFunctions.route("order_service")
+                .route(RequestPredicates.path("/api/order"), HandlerFunctions.http(orderServiceUrl))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceRoute() {
+        return GatewayRouterFunctions.route("inventory_service")
+                .route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http(inventoryServiceUrl))
+                .build();
+    }
+
+}
